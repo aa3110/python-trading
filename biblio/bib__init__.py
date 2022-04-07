b3b69a0399a8a1afd25576bee60219810a92e0ae
aa <72506090+aa3110@users.noreply.github.com>
2022-04-07 21:17:30 +0200

    bib__init__

---
+++ biblio/bib__init__.py
@@ -0,0 +1,6 @@
+from b__init__ import bus_cr,ins,layout,repl,sort,value_update
+from f__init__ import file_append,file_read,file_save,new,file_update,ini
+from p__init__ import plot_bb,plot2,plot_scatter,plot_scatter_line,plot_aroon1,col_plot,ex_lin,plot_fit_line,plot_fit_lin_channel,plot_fit_p,plot_fit_p_channel
+from constant_serie import stock,stock2
+from t__init__ import aroonhigh,aroonlow,bb,cci,cleaner,closen,cross,cross_v,fall,gap,move,pkt_r,rise,rmax,rmin,rstd,sma,smooth
+from s__init__ import serie,run_serie
